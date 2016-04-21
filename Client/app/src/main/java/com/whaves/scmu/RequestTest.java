f6e39b25a98eaf974612c9d53a67f8db4c3bfe5e
Avelino <avelinomorganti@gmail.com>
2016-04-22 00:58:05 +0100

    REMOVE

--- Client/app/src/main/java/com/whaves/scmu/RequestTest.java
+++
@@ -1,8 +0,0 @@
-package com.whaves.scmu;
-
-import com.android.volley.*;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
-
-public class RequestTest {
-}
--- README.md
+++
@@ -1,2 +0,0 @@
-# SCMU
-SCMU
